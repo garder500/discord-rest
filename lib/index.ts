79ea961172cde5cd51cf2e7937fb130dd2913459
garder500 <jeremy27.clara22@gmail.com>
2022-05-25 01:13:40 +0200

    Adding class in index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,9 @@ export * from "./structures/Client";
 export * from "./structures/Guild";
 export * from "./structures/Base";
 export * from "./structures/User";
+export * from "./structures/Message";
+export * from "./structures/Oauth2";
+export * from "./structures/Application";
 export * from "./rest/restManager";
 export * from "./types/ChannelType";
 export * from "./types/ClientType";
@@ -11,3 +14,4 @@ export * from "./types/UserType";
 export * from "./controller/ChannelController";
 export * from "./controller/UserController";
 export * from "./controller/GuildController";
+
